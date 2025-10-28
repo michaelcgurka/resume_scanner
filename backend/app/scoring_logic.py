d5bc9b443c423f81b56c386712da9cc5bc883723
Michael Gurka <michaelcgurka2@gmail.com>
2025-10-27 20:38:50 -0500

    Scoring logic improved for matching

--- backend/app/scoring_logic.py
+++ backend/app/scoring_logic.py
@@ -1,14 +1,24 @@
 from sentence_transformers import SentenceTransformer, util
 from keyword_list import tech_keywords
+import ahocorasick
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-embedding = model.encode("I am a Python engineer skilled in machine learning")
-print(embedding.shape)
+
+keyword_weight = 0.3
 
 """
 Extensive list of all tech keywords related to technical roles.
 """
 TECH_KEYWORDS = tech_keywords
+
+"""
+Build automation for keywords that will later be searched through.
+"""
+A = ahocorasick.Automation()
+for idx, key in enumerate(tech_keywords):
+    A.add_word(key, (idx, key))
+A.make_automation()
+
 
 """
 Contains the bulk of the resume scoring logic. This is where matching,
@@ -21,6 +31,13 @@ def score_resume(job_description, resume):
 
     cosine_similarity = util.cos_sim(jd_embedding, resume_embedding)
 
+    resume_matches = set([v[1] for _, v in A.iter(resume)])
+    jd_matches = set([v[1] for _, v in A.iter(job_description)])
     
+    if jd_matches:
+        keyword_score = len(resume_matches & jd_matches) / len(jd_matches)
+    else:
+        keyword_score = 0
 
-    return
+    final_score = (cosine_similarity * (1 - keyword_weight)) + (keyword_weight * keyword_score)
+    return round(final_score, 4)
