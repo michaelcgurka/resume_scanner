281d5b2e1e8395c105b19896a7b95155bd3f1ee4
Michael Gurka <michaelcgurka2@gmail.com>
2025-10-27 19:52:18 -0500

    Scoring logic beginning and keyword for matching

---
+++ backend/app/keyword_list.py
@@ -0,0 +1,13 @@
+language_keywords = ["python", "java", "c", "c++", "c#", "go", "javascript", "typescript", "ruby", 
+                "rust", "kotlin", "swift", "php", "scala", "r", "matlab", "perl", "bash", 
+                "shell scripting", "sql", "html", "css", "objective-c", "dart", "lua", "julia", "visual basic"]
+
+ml_framework_keywords = ["tensorflow", "pytorch", "scikit-learn", "pandas", "numpy", "keras", "xgboost", 
+                      "lightgbm", "catboost", "opencv", "nltk", "spacy", "gensim", "matplotlib", "seaborn", 
+                      "plotly", "fastai", "paddlepaddle", "statsmodels", "shap", "lime", "dask", 
+                      "huggingface transformers", "pytorch lightning", "jax", "mlflow", "caffe", "theano"]
+
+web_framework_keywords = ["react", "angular", "vue", "svelte", "ember", "backbone", "jquery", "bootstrap", 
+                          "tailwindcss", "django", "flask", "fastapi", "express", "spring", "spring boot", 
+                          "laravel", "ruby on rails", "symfony", "cakephp", "meteor",
+                           "next.js", "nuxt.js", "gatsby", "astro", "quasar", "adonisjs"]
---
+++ backend/app/scoring_logic.py
@@ -0,0 +1,25 @@
+from sentence_transformers import SentenceTransformer, util
+from keyword_list import tech_keywords
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+embedding = model.encode("I am a Python engineer skilled in machine learning")
+print(embedding.shape)
+
+"""
+Extensive list of all tech keywords related to technical roles. Potential for redundancy
+"""
+TECH_KEYWORDS = []
+
+"""
+Contains the bulk of the resume scoring logic. This is where matching,
+tokenization, embedding, etc takes place.
+"""
+def score_resume(job_description, resume):
+
+    jd_embedding = model.encode(job_description)
+    resume_embedding = model.encode(resume)
+
+    cosine_similarity = util.cos_sim(jd_embedding, resume_embedding)
+
+
+    return
